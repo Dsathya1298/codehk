a27f6163147e8a6cfac153b8abe92531f5cb2295
Dsathya1298 <53078146+Dsathya1298@users.noreply.github.com>
2019-07-19 14:11:56 +0530

    Update program.py

--- program.py
+++ program.py
@@ -1,7 +1,7 @@
 a=int(input())
 if (a>0):
-    print('a is positive')
+    print('a is Positive')
 elif(a<0):
-    print('a is negative')
+    print('a is Negative')
 else:
-    print('zero')
+    print('Zero')
